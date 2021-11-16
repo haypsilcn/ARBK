eae656c0bb858fad0420afd5c108e8ec91e5a625
haypsilcn <yennguyen1727@gmail.com>
2021-11-16 19:13:30 +0100

    added Praktikum02

---
+++ Praktikum02/Praktikum02/main.c
@@ -0,0 +1,18 @@
+/*
+ * Praktikum02.c
+ *
+ * Created: 16/11/2021 18:44:59
+ * Author : haypsilcn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
