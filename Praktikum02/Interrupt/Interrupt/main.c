12fe422ecd5c903360a5c4516bfa1a19245e5f5e
haypsilcn <yennguyen1727@gmail.com>
2021-11-17 04:59:45 +0100

    added Interrupt

---
+++ Praktikum02/Interrupt/Interrupt/main.c
@@ -0,0 +1,18 @@
+/*
+ * Interrupt.c
+ *
+ * Created: 17/11/2021 04:59:03
+ * Author : haypsilcn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
