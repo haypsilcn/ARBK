42b93d3a8fa3755c07d868e112cc018aa8b9bd89
haypsilcn <yennguyen1727@gmail.com>
2021-11-29 20:38:46 +0100

    added Praktikum03

---
+++ Praktikum03/Praktikum03/main.c
@@ -0,0 +1,18 @@
+/*
+ * Praktikum03.c
+ *
+ * Created: 29/11/2021 08:37:07
+ * Author : haypsilcn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
